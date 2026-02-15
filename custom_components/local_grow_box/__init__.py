06b3c0c71a1939d0c1c1576d1cc1fdf771c36405
Kai <info@low-streaming.de>
2026-02-15 22:59:05 +0100

    Update __init__.py

--- custom_components/local_grow_box/__init__.py
+++ custom_components/local_grow_box/__init__.py
@@ -355,7 +355,7 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
         hass,
         webcomponent_name="local-grow-box-panel",
         frontend_url_path="grow-room",
-        module_url="/local_grow_box/local-grow-box-panel.js?v=2.0.1",
+        module_url="/local_grow_box/local-grow-box-panel.js?v=2.0.2",
         sidebar_title="Grow Room",
         sidebar_icon="mdi:sprout",
         require_admin=False,
